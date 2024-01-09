8de96890e15c9c525d5ca743a9da7f00392fadd2
Silvia-Wachira <slywachira96@gmail.com>
2024-01-09 21:31:22 +0300

    create a card component

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="card">
+        <img className="card--image" src="./images/blacbook.jpg" alt=""/>
+        <p>Title</p>
+        <p>Price</p>
+        <p>Location</p>
+    </div>
+  )
+}
+
+export default Card
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Book Haven</h1>
+      <h2>Bring back reading books!</h2>
+      </div>
+  )
+}
+
+export default Navbar
